64fbd29860fc8b993ec86563f9a256af6a0484b1
Krjamwat <thisisforstudy191@gmail.com>
2024-11-03 00:13:17 +0700

    testagain

--- database/mypage/urls.py
+++ database/mypage/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from mypage import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index, name='index'),
     path('creative.html',views.creative),
     path('peer1.html',views.peer1),
     path('peer2.html',views.peer2),
     path('peer3.html', views.peer3),
-    path('index.html',views.index),
+    path('index.html',views.index, name='index_html'),
     path('checkscreative.html',views.room),
     path('test.html', views.test),
     path('test-creative.html', views.test_creative)
